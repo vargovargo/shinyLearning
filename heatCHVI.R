d99dcd2fc57aad4c80dcf3153208ab81b614cc7d
vargo <jvargo@gmail.com>
2017-11-29 20:43:37 -0800

    shinyEHE

--- heatCHVI.R
+++ heatCHVI.R
@@ -1,34 +1,41 @@
-rm(list=ls())
+#rm(list=ls())
 
 library(tidyverse)
 library(shiny)
 library(ggvis)
 
-ui <- fluidPage(
-  sliderInput(inputId = "CEScutoff",
-              label = "Threshold for CalEnviroscreen",
-              value = 75, min = 0, max = 100),
-  sliderInput(inputId = "HDIcutoff",
-            label = "Threshold for Health Disadvantage Index",
-            value = 75, min = 0, max = 100), 
-  plotOutput(outputId = "hist")
 
+#temp <- read.csv("~/CHVI_copy/allCHVI.csv", header=T) 
+ 
+foo <- filter(temp, ind_definition =="Projected number of extreme heat days" & geotype %in% c("CO")) %>%
+  select(reportyear, county_name, estimate, region_name)
+
+stateTable <- foo %>% group_by(reportyear) %>% 
+  summarise(stateAvg = mean(estimate, na.rm=T))
+
+avg2050 <- as.numeric(stateTable[1,2])
+avg2085 <- as.numeric(stateTable[2,2])
+
+ui <- pageWithSidebar(
+  headerPanel('Projected Number of Extreme Heat Days'),
+  sidebarPanel(
+    selectInput(inputId = "co1", label = "Select a county", choices = unique(foo$county_name)),
+    selectInput(inputId = "co2", label = "Select a second county", choices = unique(foo$county_name)),
+    selectInput(inputId = "co3", label = "Select a third county", choices = unique(foo$county_name))
+  ),
+  mainPanel(plotOutput('plot1'))
 )
-
-
+  
 server <- function(input, output, session) {
   
-  output$hist <- renderPlot({
+
+  output$plot1 <- renderPlot({
     
-    full_join(CES2, HPI2)  %>%
-      gather(CES.3.0.Percentile, ces2_pctile, key = CESind, value = percentile) %>% 
-      mutate(venn = ifelse(percentile >= input$CEScutoff & hdi_pctile >= input$HDIcutoff, "Both CES and HPI DAC",
-                           ifelse(percentile >= input$CEScutoff & hdi_pctile < input$HDIcutoff, "CES DAC only",
-                                  ifelse(percentile < input$CEScutoff & hdi_pctile >= input$HDIcutoff, "HPI DAC only","Not a DAC")))) %>%
-      ggplot(aes(x=hdi_pctile, y=percentile, color=venn)) + geom_point(size=1, alpha=0.5) + 
-      geom_hline(yintercept = input$CEScutoff, size=2, alpha=0.5) + 
-      geom_vline(xintercept = input$HDIcutoff, size=2, alpha=0.5)
-    
+  foo %>% filter(county_name %in% c(input$co1, input$co2, input$co3)) %>%
+      ggplot(aes(x=reportyear, y=estimate, fill=county_name)) + geom_bar(stat="identity", position="dodge") +
+      geom_hline(yintercept = avg2050, color="blue", alpha=0.4, linetype="dashed") + 
+      geom_hline(yintercept = avg2085, color="red", alpha=0.4, linetype="dashed") +
+      ylab("Annual Extreme Heat Days (CA average: blue = 2050, red=2085)")
     
     
   })
