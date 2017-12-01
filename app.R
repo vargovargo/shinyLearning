8600fcc34e4c6f48075ab78d02a9a98bdf9aa409
vargo <jvargo@gmail.com>
2017-11-30 19:56:50 -0800

    change variable names

--- app.R
+++ app.R
@@ -5,8 +5,8 @@ library(shiny)
 
 foo <- read.csv("https://raw.githubusercontent.com/vargovargo/CHVIr/master/heatCHVIcounty.csv", header=T)
 
-stateTable <- foo %>% group_by(reportyear) %>% 
-  summarise(stateAvg = mean(estimate, na.rm=T))
+stateTable <- foo %>% group_by(Year) %>% 
+  summarise(stateAvg = mean(Extreme.Heat.Days, na.rm=T))
 
 avg2050 <- as.numeric(stateTable[1,2])
 avg2085 <- as.numeric(stateTable[2,2])
@@ -14,9 +14,9 @@ avg2085 <- as.numeric(stateTable[2,2])
 ui <- pageWithSidebar(
   headerPanel('Projected Number of Extreme Heat Days'),
   sidebarPanel(
-    selectInput(inputId = "co1", label = "Select a county", choices = unique(foo$county_name)),
-    selectInput(inputId = "co2", label = "Select a second county", choices = unique(foo$county_name)),
-    selectInput(inputId = "co3", label = "Select a third county", choices = unique(foo$county_name))
+    selectInput(inputId = "co1", label = "Select a county", choices = unique(foo$County)),
+    selectInput(inputId = "co2", label = "Select a second county", choices = unique(foo$County)),
+    selectInput(inputId = "co3", label = "Select a third county", choices = unique(foo$County))
   ),
   mainPanel(plotOutput('plot1'))
 )
@@ -26,8 +26,8 @@ server <- function(input, output, session) {
 
   output$plot1 <- renderPlot({
     
-  foo %>% filter(county_name %in% c(input$co1, input$co2, input$co3)) %>%
-      ggplot(aes(x=reportyear, y=estimate, fill=county_name)) + geom_bar(stat="identity", position="dodge") +
+  foo %>% filter(County %in% c(input$co1, input$co2, input$co3)) %>%
+      ggplot(aes(x=Year, y=Extreme.Heat.Days, fill=County)) + geom_bar(stat="identity", position="dodge") +
       geom_hline(yintercept = avg2050, color="blue", alpha=0.4, linetype="dashed") + 
       geom_hline(yintercept = avg2085, color="red", alpha=0.4, linetype="dashed") +
       ylab("Annual Extreme Heat Days")
